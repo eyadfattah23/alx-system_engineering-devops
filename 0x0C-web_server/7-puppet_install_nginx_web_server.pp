ed77010be750a987f89c8e82bb6ad89127aca62b
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-09-28 04:07:24 +0300

    ch order

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -4,10 +4,6 @@ package { 'nginx':
   ensure => installed,
 }
 
-service { 'nginx':
-  ensure  => running,
-  require => Package['nginx'],
-}
 
 file { '/var/www/html/index.html':
   content => 'Hello World!',
@@ -18,4 +14,8 @@ file_line { 'server_config':
   after  => 'listen 80 default_server;',
   line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4/ permanent;',
 }
-custom_404
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
