f57b4c866e50e51a17e4870361681edfc2675e5e
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-10-04 04:34:35 +0300

    remove brackets

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/((?<=from:)[^\]]{1,})|((?<=to:)[^\]]{1,})|((?<=flags:)[^\]]{1,})/).join(",")
+puts ARGV[0].scan(/(?<=from:)[^\]]{1,}|(?<=to:)[^\]]{1,}|(?<=flags:)[^\]]{1,}/).join(",")
