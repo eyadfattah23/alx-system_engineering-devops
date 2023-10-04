da3e01cb90ad577ab810e398634308b85d2fc55f
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-10-04 04:26:11 +0300

    remove comment

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,3 +1,2 @@
 #!/usr/bin/env ruby
 puts ARGV[0].scan(/((?<=from:)\S*[^\]\s])|((?<=to:)\S*[^]\s])|((?<=flags:)\S*[^]\s])/).join(",")
-# look behind the 0 or more non white space character and not equal ']'
