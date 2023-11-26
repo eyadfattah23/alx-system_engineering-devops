c15fc3301844c6c96a9195a78046ada83513b8e1
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-11-26 14:32:29 +0200

    lint fix again

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
 # script to install flask
 package { 'flask_installation':
-	name => 'flask',
-	provider => 'pip3',
-	ensure => '2.1.0',
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  name     => 'flask',
 
 }
