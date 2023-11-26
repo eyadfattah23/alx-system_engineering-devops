42a6a8d3f2e541e41b70e228e907fb91dbf8ff0a
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-11-26 14:28:48 +0200

    puppet lint fix

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # create a file in /tmp.
 file { 'Create a file':
-    path    => '/tmp/school',
-    ensure  => 'present',
-    mode    => '0744',
-    owner   => 'www-data',
-    group   => 'www-data',
-    content => 'I love Puppet',
+  path    => '/tmp/school',
+  ensure  => 'present',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
