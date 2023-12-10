3b58116164bce2b7c9f12e74191d9aa32247352d
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-12-10 16:51:28 +0200

    CHANGE BACK HOSTNAME

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -27,7 +27,7 @@ file_line { 'add header':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
-  line   => 'add_header X-Served-By $hostname;',
+  line   => 'add_header X-Served-By $HOSTNAME;',
   require => Package['nginx'],
 }
 
