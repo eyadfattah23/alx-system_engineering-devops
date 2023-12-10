db87c52b35923248d1b815edc598229e7e31c1f8
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-12-10 16:35:34 +0200

    typo fix

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -28,7 +28,7 @@ file_line { 'define server name':
 file_line { 'add header':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
-  after  => ''listen 80 default_server;',
+  after  => 'listen 80 default_server;',
   line   => 'add_header X-Served-By $HOSTNAME;',
 }
 
