617fec55f7e51b3c912b9b4493ec529b5d038435
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-01-25 04:37:46 +0200

    fix typo

--- 0x14-mysql/tyrell_corp_creation.sql
+++ 0x14-mysql/tyrell_corp_creation.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE IF NOT EXISTS tyrell_corp;
 USE tyrell_corp;
-CREATE TABLE IF NOT EXISTS 'nexus6' 
+CREATE TABLE IF NOT EXISTS nexus6 
 (id int, name varchar(255));
 INSERT INTO nexus6
 VALUES (1,'EDDY');
