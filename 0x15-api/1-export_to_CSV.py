b0db879629dbf14507b73953e666be856689e298
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-02-03 10:41:13 +0200

    update to export to csv

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+'''script that uses REST API,
+for a given employee ID,
+to return information about his/her todo list progress.
+and export to a csv file USER_ID.csv'''
+
+import csv
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/'
+
+    userId = int(argv[1])
+    user = requests.get(url + f"users/{userId}").json()
+
+    user_name = user.get('name')
+
+    file_name = f"{userId}.csv"
+    total_tasks = 0
+    done_tasks = 0
+
+    dtasks_titles = []
+    tasks = requests.get(url + "todos/").json()
+
+    with open(file_name, 'w', newline='') as file:
+        writer = csv.writer(file, quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        for task in tasks:
+            if task.get('userId') == userId:
+                # Format: "USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
+                writer.writerow([f"{userId}", f"{user_name}", f"{str(task.get('completed'))}",
+                                 f"{task.get('title')}"])
--- 0x15-api/README.md
+++ 0x15-api/README.md
@@ -1,3 +1,5 @@
 # API
 
 [requests detailed video](https://www.youtube.com/watch?v=tb8gHvYlCFs&t=300s)
+
+[CSV File in Python](https://www.scaler.com/topics/how-to-create-a-csv-file-in-python/)
