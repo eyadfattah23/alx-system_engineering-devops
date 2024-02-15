e556b052c1168a8000207616826e5ce9d6409f36
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-02-15 15:04:25 +0200

    try

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -15,7 +15,7 @@ def number_of_subscribers(subreddit):
 
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {
-        'User-Agent': 'myapp/1.0'}
+        'User-Agent': 'app/1.0'}
     response = requests.get(url, headers=headers)
     if response.status_code != 200:
         return 0
