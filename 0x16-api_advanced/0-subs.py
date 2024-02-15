14a8a05adbcff13da026dbd92e334d2ea6be452a
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-02-15 14:50:58 +0200

    remove excess print line

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -19,7 +19,6 @@ def number_of_subscribers(subreddit):
             AppleWebKit/537.36 (KHTML, like Gecko) \
                 Chrome/58.0.3029.110 Safari/537.3'}
     response = requests.get(url, headers=headers)
-    print(response)
     if response.status_code != 200:
         return 0
     data = response.json()
