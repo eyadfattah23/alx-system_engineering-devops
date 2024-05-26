d70f28e1a66adfb0ab0ea546d19f3fcbc6fa4184
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-05-27 02:51:55 +0300

    code should not be executed when imported

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,27 +7,28 @@ to return information about his/her todo list progress.'''
 import requests
 from sys import argv
 
+if __name__ == "__main__":
+    user_id = int(argv[1])
+    user = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)).json()
 
-user_id = int(argv[1])
-user = requests.get(
-    'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)).json()
+    todos = requests.get(
+        'https://jsonplaceholder.typicode.com/todos/').json()
 
-todos = requests.get(
-    'https://jsonplaceholder.typicode.com/todos/').json()
+    user_name = user.get('name')
 
-user_name = user.get('name')
+    todos_total = 0
+    todos_done = 0
+    done_list = []
+    for task in todos:
+        if task.get('userId') == user_id:
+            todos_total += 1
+            if task.get('completed'):
+                todos_done += 1
+                done_list.append(task.get('title'))
 
-todos_total = 0
-todos_done = 0
-done_list = []
-for task in todos:
-    if task.get('userId') == user_id:
-        todos_total += 1
-        if task.get('completed'):
-            todos_done += 1
-            done_list.append(task.get('title'))
+    print(
+        f'Employee {user_name} is done with tasks({todos_done}/{todos_total}):')
 
-print(f'Employee {user_name} is done with tasks({todos_done}/{todos_total}):')
-
-for title in done_list:
-    print(f'\t {title}')
+    for title in done_list:
+        print(f'\t {title}')
