3c15c05a329f9591413acb1ed195e2fa494efbf3
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-05-27 05:13:58 +0300

    easier approach

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -2,32 +2,32 @@
 '''script that uses REST API,
 for a given employee ID,
 to return information about his/her todo list progress.
-and export to a csv file USER_ID.json'''
+and export to a json file USER_ID.json'''
 
 import json
 import requests
 from sys import argv
+
 if __name__ == "__main__":
-    url = 'https://jsonplaceholder.typicode.com/'
+    user_id = int(argv[1])
+    user = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)).json()
 
-    userId = int(argv[1])
-    user = requests.get(url + f"users/{userId}").json()
+    todos = requests.get(
+        'https://jsonplaceholder.typicode.com/todos/').json()
 
-    user_name = user.get('username')
+    userName = user.get('username')
 
-    file_name = f"{userId}.json"
-    total_tasks = 0
-    done_tasks = 0
+    user_data = {f"{user_id}": []}
 
-    tasks_list = []
-    tasks = requests.get(url + "todos/").json()
+    for task in todos:
+        if task.get('userId') == user_id:
+            task_dict = {
+                "task": f"{task.get('title')}",
+                "completed": task.get('completed'),
+                "username": userName}
 
-    with open(file_name, 'w', newline='') as file:
-        for task in tasks:
-            if task.get('userId') == userId:
-                tasks_list.append({"task": task.get('title'),
-                                   "completed": task.get('completed'),
-                                   "username": user_name})
-        json_dict = {f"{userId}": tasks_list}
-        json.dump(json_dict, file)
-        pass
+            user_data[f"{user_id}"].append(task_dict)
+
+    with open(f'{user_id}.json', 'w') as file:
+        json.dump(user_data, file)
