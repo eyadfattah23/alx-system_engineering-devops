aa9c8dd4a9c3fccfeb56569f3ed236c58f836ecd
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-08-20 16:18:42 +0300

    revert (any errors are not from my side)

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,21 +1,42 @@
 #!/usr/bin/python3
-"""queries the Reddit API and prints the titles of the first 10 hot
-    posts listed for a given subreddit."""
+''' function that queries the Reddit API
+prints the titles of the first 10 hot post
+listed for a given subreddit.'''
+
 import requests
+
+'''url = 'https://www.reddit.com/r/programming/about.json'
+
+r = requests.get(url, auth=('L6IfrPbBUX5qkFAj4MqLdg',
+                 'TwZviLWscqEJgWQ0PRYfmgOy_T03NA'))
+
+print(r.json()['data']['subscribers'])
+'''
 
 
 def top_ten(subreddit):
-    """querie Reddit API and prints first 10 hot posts"""
+    '''prints the titles of the first 10 hot post'''
 
-    headers = {'User-Agent': 'MyPythonScript/1.0'}
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    request = requests.get(url=url, headers=headers, allow_redirects=False)
-    if request.status_code == 200:
-        hot_ten = request.json().get('data').get('children')
-        if not hot_ten:
-            return print("None")
-        else:
-            for child in hot_ten:
-                print(child.get('data').get("title"))
-    else:
-        return print("None")
+    URL = 'https://www.reddit.com/r/' + subreddit + '/hot.json'
+    if not subreddit:
+        print(None)
+        return
+    try:
+        headers = {
+            'User-Agent': 'myapp/1.0'}
+        r = requests.get(URL,
+                         headers=headers)
+
+        if r.status_code != 200:
+            print(None)
+            return
+
+        i = 0
+        for post in r.json().get('data').get('children'):
+            if i == 10:
+                break
+            print(post.get('data').get('title'))
+            i += 1
+    except Exception as e:
+        print('error: ', e)
+        print(None)
