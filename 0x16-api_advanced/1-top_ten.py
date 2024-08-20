916770676e0848a1c26b053d168a5d1df2ffe61e
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-08-20 16:10:17 +0300

    new approach

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -2,17 +2,42 @@
 ''' function that queries the Reddit API
 prints the titles of the first 10 hot post
 listed for a given subreddit.'''
+
 import requests
+
+'''url = 'https://www.reddit.com/r/programming/about.json'
+
+r = requests.get(url, auth=('L6IfrPbBUX5qkFAj4MqLdg',
+                 'TwZviLWscqEJgWQ0PRYfmgOy_T03NA'))
+
+print(r.json()['data']['subscribers'])
+'''
 
 
 def top_ten(subreddit):
     '''prints the titles of the first 10 hot post'''
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
-    headers = {
-        'User-Agent': 'myapp/1.0'}
-    response = requests.get(url, headers=headers)
-    if response.status_code != 200:
+
+    URL = 'https://www.reddit.com/r/' + subreddit + '/hot.json'
+    if not subreddit:
         print(None)
-    data = response.json()['data']['children']
-    for post in data:
-        print(post['data']['title'])
+        return
+    try:
+        headers = {
+            'User-Agent': 'myapp/1.0'}
+        r = requests.get(URL, auth=('L6IfrPbBUX5qkFAj4MqLdg',
+                                    'TwZviLWscqEJgWQ0PRYfmgOy_T03NA'),
+                         headers=headers)
+
+        if r.status_code != 200:
+            print(None)
+            return
+
+        i = 0
+        for post in r.json()['data']['children']:
+            if i == 10:
+                break
+            print(post['data']['title'])
+            i += 1
+    except Exception as e:
+        print('error: ', e)
+        print(None)
